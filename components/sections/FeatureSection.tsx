1ccccfef5ba5368937d4502bd577e1f232a68141
beyzaisil <beyzaisilbaytemir@gmail.com>
2026-07-27 17:36:37 +0300

    feat(features): add feature section components

---
+++ components/sections/FeatureSection.tsx
@@ -0,0 +1,29 @@
+import { features } from "@/lib/content";
+import { FeatureCard } from "@/components/sections/FeatureCard";
+
+
+export function FeatureSection() {
+    return(
+<section>
+<div>
+{features.map((feature)=> (
+    <FeatureCard 
+        key={feature.id}
+         id = {feature.id}
+        icon = {feature.icon}
+        title={feature.title}
+        description={feature.description}
+    />
+)
+
+)}
+</div>
+
+
+</section>
+
+
+    );
+
+
+}
