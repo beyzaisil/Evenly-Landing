8d30c13d57c687ec2a2d2639226db053e27bc2fa
MuhittinDayan <mhtndyn.34@gmail.com>
2026-07-28 09:19:31 +0300

    fix(featureCard): rename title prop to titles for consistency

--- components/sections/FeatureCard.tsx
+++ components/sections/FeatureCard.tsx
@@ -2,7 +2,7 @@ import { Feature } from "@/types/features";
 import { UsersRound,Ticket, Icon,CalendarClock } from "lucide-react";
 import {features  } from "@/lib/content";
 
-    export function FeatureCard({icon:Icon,title,description}:Feature) {
+    export function FeatureCard({icon:Icon,title:titles,description}:Feature) {
         return(
         <div className="flex flex-col justify-between rounded-3xl border border-gray-200 bg-white p-8 shadow-sm transition-all duration-300 hover:-translate-y-2 hover:shadow-xl ">
       <div>
@@ -12,7 +12,7 @@ import {features  } from "@/lib/content";
         </div>
 
         <h3 className="mb-4 text-3xl font-bold text-slate-900">
-          {title}
+          {titles}
         </h3>
 
         <p className="leading-8 text-slate-600 ">
