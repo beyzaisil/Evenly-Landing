09a325f3fb133044f4a90bc6876d2e871dea6801
beyzaisil <beyzaisilbaytemir@gmail.com>
2026-07-30 13:44:48 +0300

    refactor: reorganize feature components and data

--- components/sections/FeatureSection.tsx
+++ components/sections/FeatureSection.tsx
@@ -1,29 +1,23 @@
-import { features } from "@/lib/content";
-import { FeatureCard } from "@/components/sections/FeatureCard";
+import { features } from "@/data/content";
+import { FeatureCard } from "@/components/blocks/cards/FeatureCard";
 
 
 export function FeatureSection() {
-    return(
-<section>
-<div className="grid grid-cols-4 gap-6">
-{features.map((feature)=> (
-    <FeatureCard 
-        key={feature.id}
-         id = {feature.id}
-        icon = {feature.icon}
-        title={feature.title}
-        description={feature.description}
-    />
-)
-
-)}
-</div>
-
-
-</section>
-
-
+    return (
+        <section>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 ">
+                
+                {features.map((feature) => (
+                    <FeatureCard
+                        key={feature.id}
+                        id={feature.id}
+                        icon={feature.icon}
+                        title={feature.title}
+                        description={feature.description}
+                    />
+                )
+                )}
+            </div>
+        </section>
     );
-
-
 }
